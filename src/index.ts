7a0de6c6d45438de7dbf858b0f20a7862e6d2c0c
Aneurin Barker Snook <a@aneur.in>
2023-03-20 09:45:11 +0000

    feat: add logging

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@
 // Use of this source code is governed by a GNU GPL-style license
 // that can be found in the LICENSE.md file. All rights reserved.
 
+import dotenv from 'dotenv'
 import main from './main'
 
-main()
+dotenv.config()
+
+main({
+  log: {
+    level: process.env.LOG_LEVEL || 'warn'
+  }
+})
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,33 @@
 // Use of this source code is governed by a GNU GPL-style license
 // that can be found in the LICENSE.md file. All rights reserved.
 
-const main = async () => {
-  console.log('Hello, world')
+import { Log, LogLevelFromString, StdioAdaptor } from '@edge/log'
+
+export type Config = {
+  log: {
+    level: string
+  }
+}
+
+export type Context = {
+  config: Config
+  log: Log
+}
+
+const createLogger = ({ config }: Context) => {
+  const log = new Log()
+  log.use(new StdioAdaptor(true))
+  log.setLogLevel(LogLevelFromString(config.log.level))
+  return log
+}
+
+const main = async (config: Config) => {
+  const ctx = <Context>{ config }
+
+  ctx.log = createLogger(ctx)
+  ctx.log.info('initialized logger')
+
+  ctx.log.error('WIP')
 }
 
 export default main
