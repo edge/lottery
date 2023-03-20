9da569d6bb1793101fdc26e33972ea6f27af34c4
Aneurin Barker Snook <a@aneur.in>
2023-03-20 09:14:11 +0000

    chore: init ts project

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// Copyright (C) 2023 Edge Network Technologies Limited
+// Use of this source code is governed by a GNU GPL-style license
+// that can be found in the LICENSE.md file. All rights reserved.
+
+import main from './main'
+
+main()
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// Copyright (C) 2023 Edge Network Technologies Limited
+// Use of this source code is governed by a GNU GPL-style license
+// that can be found in the LICENSE.md file. All rights reserved.
+
+const main = async () => {
+  console.log('Hello, world')
+}
+
+export default main
