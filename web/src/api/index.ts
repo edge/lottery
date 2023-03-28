1381a7973fee03096dabf372bb6128485c26770d
Aneurin Barker Snook <a@aneur.in>
2023-03-28 09:10:21 +0100

    chore: add license comments to vue scripts

--- web/src/api/index.ts
+++ web/src/api/index.ts
@@ -1,3 +1,7 @@
+// Copyright (C) 2023 Edge Network Technologies Limited
+// Use of this source code is governed by a GNU GPL-style license
+// that can be found in the LICENSE.md file. All rights reserved.
+
 import type { SuperAgentRequest } from 'superagent'
 
 export type RequestCallback = (r: SuperAgentRequest) => SuperAgentRequest
--- web/src/views/NewDrawView.vue
+++ web/src/views/NewDrawView.vue
@@ -1,3 +1,9 @@
+<!--
+// Copyright (C) 2023 Edge Network Technologies Limited
+// Use of this source code is governed by a GNU GPL-style license
+// that can be found in the LICENSE.md file. All rights reserved.
+-->
+
 <script setup lang="ts">
 import * as draws from '@/api/draws'
 import * as earnings from '@/api/earnings'
